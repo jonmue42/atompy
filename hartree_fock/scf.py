8496624c09c1e97621594d550026ea39bc48bcb5
Jonas Müller <jonmue@uni-bremen.de>
2024-04-27 18:29:43 +0200

    fixed bug. for the summing for building the density matrix I used the Number of basis functions, but for I needed to use the number of electrons in the system instead

--- hartree_fock/scf.py
+++ hartree_fock/scf.py
@@ -6,6 +6,7 @@ class RHF():
     def __init__(self, molecule):
         self.molecule = molecule
         self.Nbas = molecule.nao 
+        self.nelectron = molecule.nelectron
         self.overlap = molecule.intor('int1e_ovlp')
         self.kinetic = molecule.intor('int1e_kin')
         self.nuclear = molecule.intor('int1e_nuc')
@@ -14,17 +15,19 @@ class RHF():
         print('Initialized RHF')
         print('Overlap')
         print(self.overlap)
+        print('Hcore')
+        print(self.Hcore)
 
     def __call__(self, initial_c, tol, max_iter):
-        density = self._density(initial_c)
-        Fock = self._Fock(density)
         energy, c = self._calculate_cycle(initial_c, self.overlap)
         for iteration in range(max_iter):
             energy_new, c_new = self._calculate_cycle(c, self.overlap)
-            if np.allclose(energy, energy_new, atol=tol, rtol=0.0):
+            if np.allclose(energy, energy_new, atol=tol, rtol=0.0) and np.allclose(c, c_new, atol=tol, rtol=0.0):
                 print('Converged')
                 density = self._density(c_new)
                 total_elec_energy = self._total_elec_energy(density, self._Fock(density))
+                print('Fock')
+                print(self._Fock(density))
                 return energy_new, c_new, total_elec_energy
             else:
                 energy = energy_new
@@ -48,15 +51,23 @@ class RHF():
                 Fock[i, j] += Hcore[i, j]
                 for k in range(int(self.Nbas)):
                     for l in range(int(self.Nbas)):
-                        Fock[i, j] += density[k, l] * (self.two_electron[i, j, k, l] - 0.5 * self.two_electron[i, l, k, j])
+                        Fock[i, j] += density[k, l] * (self.two_electron[i, j, k, l] - 0.5 * self.two_electron[i, k, j, l])
         return Fock
 
     def _density(self, c):
         density = np.zeros((self.Nbas, self.Nbas))
+        norm = np.zeros(self.Nbas)
+        for k in range(int(self.Nbas)):
+            for p in range(self.Nbas):
+                for q in range(self.Nbas):
+                    norm[k] += c[p, k] * c[q, k] * self.overlap[p, q]
+        print('Norm')
+        print(norm)
         for i in range(self.Nbas):
             for j in range(self.Nbas):
-                for k in range(int(self.Nbas/2)):
-                    density[i, j] += 2 * c[i, k] * c[j, k]
+                for k in range(int(self.nelectron/2)):
+                    density[i, j] +=2 * c[i, k] * c[j, k]
+
         return density
     
     def _total_elec_energy(self, density, Fock):
@@ -72,99 +83,3 @@ class RHF():
 
 
 
-class scf_hf():
-
-    def __init__(self, molecule):
-        self.molecule = molecule
-        self.Nbas = molecule.nao 
-        self.overlap = molecule.intor('int1e_ovlp')
-        self.kinetic = molecule.intor('int1e_kin')
-        self.nuclear = molecule.intor('int1e_nuc')
-        self.Hcore = self.kinetic + self.nuclear
-        self.two_electron = molecule.intor('int2e')
-
-    def _calculate_cycle(self, c, S):
-        density = self._calculate_density(c)
-        Fock = self._Fock_matrix(density)
-        overlap = S
-        energy, c = eigh(Fock, overlap) 
-        return energy, c 
-
-    def _Fock_matrix(self, density):
-        #_Fock_matrix = T + V + Vcoulomb - Vexchange
-        # F_mu,nu = Hcore_mu,nu + G_mu,nu
-        #Fock = self._calculate_Hcore()
-
-        coulomb = self._calculate_coulomb(density)
-        exchange = self._calculate_exchange(density)
-        Fock = self.kinetic + self.nuclear + coulomb - 0.5 * exchange
-        return Fock
-
-    def _calculate_Hcore(self):
-        return self.kinetic + self.nuclear
-
-    def _calculate_coulomb(self, density):
-        coulomb = np.zeros((self.Nbas, self.Nbas))
-        for i in range(self.Nbas):
-            for j in range(self.Nbas):
-                for k in range(self.Nbas):
-                    for l in range(self.Nbas):
-                        coulomb[i, j] += density[k, l] * self.two_electron[i, j, k, l]
-        return coulomb
-    
-    def _calculate_exchange(self, density):
-        exchange = np.zeros((self.Nbas, self.Nbas))
-        for i in range(self.Nbas):
-            for j in range(self.Nbas):
-                for k in range(self.Nbas):
-                    for l in range(self.Nbas):
-                        exchange[i, j] += density[k, l] * self.two_electron[i, l, k, j]
-        return exchange
-
-    def _calculate_density(self, c):
-        print('Calculating density')
-        print('c')
-        print(c)
-        density = np.zeros((self.Nbas, self.Nbas))
-        for i in range(self.Nbas):
-            for j in range(self.Nbas):
-                for k in range(int(self.Nbas/2)):
-                    density[i, j] += 2 * c[i, k] * c[j, k]
-        return density
-
-    def _calculate_total_energy(self, density, Hcore, Fock):
-        total_energy = 0
-        for mu in range(int(self.Nbas)):
-            for nu in range(int(self.Nbas)):
-                total_energy += 0.5 * density[nu, mu] * (Hcore[mu, nu] + Fock[mu, nu])
-        return total_energy
-
-    def __call__(self, initial_c, tol, max_iter):
-        print('Called SCF')
-        print('#################################################')
-        density = self._calculate_density(initial_c)
-        print('Initial density')
-        print(density)
-        energy, c = self._calculate_cycle(initial_c, self.overlap)
-        for iteration in range(max_iter):
-            energy_new, c_new = self._calculate_cycle(c, self.overlap)
-            if np.allclose(energy, energy_new, atol=tol, rtol=0.0):
-                print('Converged')
-                density = self._calculate_density(c_new)
-                total_energy = self._calculate_total_energy(density, self.Hcore, self._Fock_matrix(density))
-                return energy_new, c_new, total_energy
-            else:
-                energy = energy_new
-                c = c_new
-        density = self._calculate_density(c)
-        total_energy = self._calculate_total_energy(density, self.Hcore, self._Fock_matrix(density))
-        return energy, c, total_energy
-            
-
-
-
-
-
-
-
-       
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from basis_sets.gaussian import GaussianBasis
 from structure import atom, molecule
-from hartree_fock.scf import scf_hf, RHF
+from hartree_fock.scf import RHF
 import typing
 import numpy as np 
 
@@ -12,22 +12,22 @@ from pyscf import gto, scf
 # calculate the overlap matrix etc.
 # perform the SCF calculation
 
-#H2 = gto.M(atom='H 0 0 0; H 0 0 1.4', basis='stop-3g', unit='Bohr')
-#print(H2.atom_coords())
-#
-#scf = RHF(H2)
-#
-#initial_guess = np.zeros((scf.Nbas, scf.Nbas))
-#energy, c, total_elec_energy = scf(initial_guess, tol=1E-10, max_iter=100000000000)
-#
-#print('Energy')
-#print(energy)
-#print('c')
-#print(c)
-#print('total electronic energy')
-#print(total_elec_energy)
-#print('total energy')
-#print(total_elec_energy + H2.energy_nuc())
+H2 = gto.M(atom='H 0 0 0; H 0 0 1.4', basis='sto-3g', unit='Bohr')
+print(H2.atom_coords())
+
+scf = RHF(H2)
+
+initial_guess = np.zeros((scf.Nbas, scf.Nbas))
+energy, c, total_elec_energy = scf(initial_guess, tol=1E-10, max_iter=100000000000)
+
+print('Energy')
+print(energy)
+print('c')
+print(c)
+print('total electronic energy')
+print(total_elec_energy)
+print('total energy')
+print(total_elec_energy + H2.energy_nuc())
 
 #
 #H = gto.M(atom='He 0 0 0', basis='sto-3g', unit='Bohr')
@@ -43,18 +43,20 @@ from pyscf import gto, scf
 #print('total energy')
 #print(total_elec_energy + CO.energy_nuc())
 
-#CO = gto.M(atom='C 0 0 0; O 0 0 2.166', basis='sto-3g', unit='Bohr')
-#scf = RHF(CO)
-#initial_guess = np.zeros((scf.Nbas, scf.Nbas))
-#energy, c, total_elec_energy = scf(initial_guess, tol=1e-10, max_iter=1000)
-#print('Energy')
-#print(energy)
-#print('total electronic energy')
-#print(total_elec_energy)
-#print('total energy')
-#print(total_elec_energy + CO.energy_nuc())
-#print('Nbas')
-#print(scf.Nbas)
+CO = gto.M(atom='C 0 0 0; O 0 0 2.166', basis='sto-3g', unit='Bohr')
+scf = RHF(CO)
+initial_guess = np.zeros((scf.Nbas, scf.Nbas))
+energy, c, total_elec_energy = scf(initial_guess, tol=1e-2, max_iter=1000)
+print('Energy')
+print(energy)
+print('total electronic energy')
+print(total_elec_energy)
+print('total energy')
+print(total_elec_energy + CO.energy_nuc())
+print('Nbas')
+print(scf.Nbas)
+print('number of electrons')
+print(CO.nelectron)
 
 #N2 = gto.M(atom='N 0 0 0; N 0 0 2.074', basis='sto-3g', unit='Bohr')
 #
@@ -67,17 +69,17 @@ from pyscf import gto, scf
 #print(total_energy)
 #print('total energy')
 #print(total_energy + N2.energy_nuc())
-
-H2O = gto.M(atom='O 0 0 0; H 0 0 1.809; H 1.809 0 0', basis='sto-3g', unit='Bohr')
-scf = RHF(H2O)
-print('Nbas')
-print(scf.Nbas)
-print('overlapp')
-print(scf.overlap)
-initial_guess = np.zeros((scf.Nbas, scf.Nbas))
-energy, c, total_elec_energy = scf(initial_guess, tol=1e-10, max_iter=1000)
-print('Energy')
-print(energy)
-print('total electronic energy')
-print(total_elec_energy)
-
+#
+#H2O = gto.M(atom='O 0 0 0; H 0 0 1.809; H 1.809 0 0', basis='sto-3g', unit='Bohr')
+#scf = RHF(H2O)
+#print('Nbas')
+#print(scf.Nbas)
+#print('overlapp')
+#print(scf.overlap)
+#initial_guess = np.zeros((scf.Nbas, scf.Nbas))
+#energy, c, total_elec_energy = scf(initial_guess, tol=1e-10, max_iter=1000)
+#print('Energy')
+#print(energy)
+#print('total electronic energy')
+#print(total_elec_energy)
+#
